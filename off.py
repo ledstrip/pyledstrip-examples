23ab47ca123a9f5a2d3d773e715f72d5ef5e229f
Michael Cipold <github@cipold.de>
2017-09-25 19:12:49 +0200

    update to latest example style

--- off.py
+++ off.py
@@ -1,5 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+import argparse
+
 from pyledstrip import LedStrip
 
-if __name__ == "__main__":
-	strip = LedStrip()
+
+def main(args):
+	strip = LedStrip(args=args)
 	strip.off()
+
+
+if __name__ == '__main__':
+	parser = argparse.ArgumentParser(description='Example code for pyledstrip.')
+	LedStrip.add_arguments(parser)
+	main(parser.parse_args())
